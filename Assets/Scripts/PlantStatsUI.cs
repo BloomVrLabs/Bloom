f8086fe6e9fff222524c24f04b1b245a569edf0b
Kevin Burgos <36522318+Kevin-Burgos-9@users.noreply.github.com>
2022-12-12 15:58:21 -0400

    Changes

--- Assets/Scripts/PlantStatsUI.cs
+++ Assets/Scripts/PlantStatsUI.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,5 +7,22 @@ public class PlantStatsUI : MonoBehaviour
     public UserInterfaceBar healthBar;
     public UserInterfaceBar waterBar;
     public UserInterfaceBar sunBar;
-    
+
+    public Transform rayOrigin;
+
+
+    public void Update()
+    {
+        
+        RaycastHit hit;
+        
+        if (Physics.Raycast(rayOrigin.position, rayOrigin.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(rayOrigin.position, rayOrigin.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+        }
+        else
+        {
+            Debug.DrawRay(rayOrigin.position, rayOrigin.TransformDirection(Vector3.forward) * 1000, Color.white);
+        }
+    }
 }
